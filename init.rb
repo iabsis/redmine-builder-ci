2e15e7a57885d36efcf466d546c7e169f4302b17
Olivier Bitsch <olivier.b@iabsis.com>
2021-06-04 15:34:36 +0200

    New builds entry into menu for admin

--- init.rb
+++ init.rb
@@ -6,6 +6,7 @@ Redmine::Plugin.register :continous_integration do
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
   menu :project_menu, :builds, { :controller => 'builds', :action => 'index' }, :caption => 'Builds', :after => :repository, :param => :project_id
+  menu :top_menu, :builds, { :controller => 'builds', :action => 'all' }, :caption => 'Builds'
   project_module :builds do
     permission :view_builds, :builds => [:view, :index]
     permission :new_builds, :builds => [:new, :update]
